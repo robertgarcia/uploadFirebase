a33d9cae76af562bb4866350b32df7ea92e4bafe
Roberto D. Garcia <robertdavid.mgt@gmail.com>
2019-08-27 15:07:25 -0600

    Agregar validaciones en la directiva

--- src/app/directives/ng-drop-files.directive.ts
+++ src/app/directives/ng-drop-files.directive.ts
@@ -1,10 +1,12 @@
 import { Directive, EventEmitter, ElementRef, HostListener, Input, Output } from '@angular/core';
+import { FileItem } from '../models/file-item';
 
 @Directive({
   selector: '[appNgDropFiles]'
 })
 export class NgDropFilesDirective {
 
+  @Input() archivos: FileItem[] = [];
   @Output() mouseSobre: EventEmitter<boolean> = new EventEmitter();
   constructor() { }
 
@@ -16,6 +18,34 @@ export class NgDropFilesDirective {
   @HostListener('dragleave', ['$event'])
   public onDragLeave( event: any ) {
     this.mouseSobre.emit(false);
+  }
+
+  // Validaciones
+  private prevenirDetener(event) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  private verficarArchivo(nombre: string): boolean {
+    for ( const archivo of this.archivos ) {
+      if (archivo.nombreArchivo === nombre) {
+        console.log(`El archivo ${nombre} ya existe!`);
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private veficarTipo(tipo: string): boolean {
+    return ( tipo === '' || tipo === undefined ) ? false : tipo.startsWith('image');
+  }
+
+  private verficarDrop(archivo: File): boolean {
+    if (this.verficarArchivo(archivo.name) && this.veficarTipo(archivo.type)) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
 }
