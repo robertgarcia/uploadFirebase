b36d8b52221a0f52314d1d612b4d0df02d70e906
Roberto D. Garcia <robertdavid.mgt@gmail.com>
2019-08-27 14:17:58 -0600

    Diseño del componente de carga

--- src/app/components/carga/carga.component.ts
+++ src/app/components/carga/carga.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FileItem } from '../../models/file-item';
+import { UploadFilesService } from '../../services/upload-files.service';
 
 @Component({
   selector: 'app-carga',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CargaComponent implements OnInit {
 
-  constructor() { }
+  archivos: FileItem[] = [];
+  constructor(
+    public service: UploadFilesService
+  ) { }
 
   ngOnInit() {
+  }
+
+  cargarArchivos() {
+    this.service.cargarImagenesFirebase(this.archivos);
   }
 
 }
--- src/app/services/upload-files.service.ts
+++ src/app/services/upload-files.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { FileItem } from '../models/file-item';
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +14,9 @@ export class UploadFilesService {
 
   private guardarImagen( imagen: { nombre: string, url: string } ) {
     this.db.collection(`/${ this.CARPETA_IMAGENES }`).add(imagen);
+  }
+
+  cargarImagenesFirebase(imagenes: FileItem[]) {
+    console.log(imagenes);
   }
 }
