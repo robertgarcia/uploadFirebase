2f38dcfd95f8690c3a2b4015dd9c1dcfeaf1771f
Roberto D. Garcia <robertdavid.mgt@gmail.com>
2019-08-28 15:30:57 -0600

    Cargar imágenes a Firebase-Storege

--- src/app/services/upload-files.service.ts
+++ src/app/services/upload-files.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { FileItem } from '../models/file-item';
+import * as firebase from 'firebase';
 
 @Injectable({
   providedIn: 'root'
@@ -13,10 +14,32 @@ export class UploadFilesService {
   ) { }
 
   private guardarImagen( imagen: { nombre: string, url: string } ) {
-    this.db.collection(`/${ this.CARPETA_IMAGENES }`).add(imagen);
+    this.db.collection(`${ this.CARPETA_IMAGENES }`).add(imagen);
   }
 
   cargarImagenesFirebase(imagenes: FileItem[]) {
-    console.log(imagenes);
+    const storageRef = firebase.storage().ref();
+    for ( const item of imagenes) {
+      item.estado = true;
+      if (item.progreso >= 100) {
+        continue;
+      }
+      const uploadTask: firebase.storage.UploadTask = storageRef.child(`${ this.CARPETA_IMAGENES }/${ item.nombreArchivo }`).put(item.archivo);
+      uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
+        (snapshot: firebase.storage.UploadTaskSnapshot) => item.progreso = ((snapshot.bytesTransferred) / snapshot.totalBytes ) * 100,
+        (error) => console.error('Error al subir', error),
+        () => {
+          console.log('Imagen cargada correctamente!');
+          uploadTask.snapshot.ref.getDownloadURL()
+          .then((data: string) => {
+            item.estado = false;
+            item.url = data;
+            this.guardarImagen({
+              nombre: item.nombreArchivo,
+              url: item.url
+            });
+          }).catch(err => console.error('Se genero un error al obtener el url de la imagen en firebase', err));
+        });
+    }
   }
 }
